51e6d4243022a7dc2d6b8e99bb341e3006c341cc
Bykonya Ilya <illya.aleksandrowich@mail.ru>
2024-03-29 16:16:50 +0300

    Add mireaproject module

---
+++ app/mireaproject/src/main/java/com/mirea/bykonyaia/mireaproject/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mirea.bykonyaia.mireaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
